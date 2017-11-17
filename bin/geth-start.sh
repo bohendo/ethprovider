5daee6dd8902caf84f842613fa973282fb71d6cc
Bo Henderson <bohende@gmail.com>
2017-11-16 20:48:29 -0800

    Limit geth cache memory

--- bin/geth-start.sh
+++ bin/geth-start.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+log=$HOME/geth.log
+ipc=$HOME/geth.ipc
+
 # Setup a convenient error handler
 function err { >&2 echo "Error: $1"; exit 1; }
 
 # Don't start geth if it's already started
-[[ -e "$HOME/geth.ipc" ]] && err "geth is already running"
+[[ -e "$ipc" ]] && err "geth is already running"
 
 # Get our internal ip address
 privateip=`ifconfig eth1 | grep 'inet addr' | awk '{print $2;exit}' | sed 's/addr://'`
 
 # Add a division & timestamp in our geth log
-echo "========== `date`" >> $HOME/geth.log
+echo "========== `date`" >> $log
 
 # Start geth w rpc enabled
-geth --identity bonet --rpc --rpcaddr $privateip --rpcport 3993 --ipcpath $HOME/geth.ipc 2>> $HOME/geth.log &
+geth --cache=4096 --identity=bonet --rpc --rpcaddr=$privateip --rpcport=3993 --ipcpath=$ipc 2>> $log &
 
--- bin/tgeth-start.sh
+++ bin/tgeth-start.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+log=$HOME/tgeth.log
+ipc=$HOME/geth.ipc
+
 # Setup a convenient error handler
 function err { >&2 echo "Error: $1"; exit 1; }
 
 # Don't start tgeth if it's already started
-[[ -e "$HOME/tgeth.ipc" ]] && err "tgeth is already running"
+[[ -e "$ipc" ]] && err "tgeth is already running"
 
 # Get our public ip address
 publicip=`ifconfig eth0 | grep 'inet addr' | awk '{print $2;exit}' | sed 's/addr://'`
 
 # Add a division & timestamp in our tgeth log
-echo "========== `date`" >> $HOME/tgeth.log
+echo "========== `date`" >> $log
 
 # Start tgeth w rpc enabled
-tgeth --rpc --rpcaddr $publicip --rpcport 8545 --rpccorsdomain "*" --ipcpath $HOME/tgeth.ipc 2>> $HOME/tgeth.log &
+tgeth --cache=512--rpc --rpcaddr=$publicip --rpcport=8545 --rpccorsdomain="*" --ipcpath=$ipc 2>> $log &
 
