6a41c432d68e598f1a3eb459661ac136026f53be
Bo Henderson <bohende@gmail.com>
2017-11-16 19:49:53 -0800

    Bigger swap 2GB->6GB

--- bin/swap-init.sh
+++ bin/swap-init.sh
@@ -3,7 +3,7 @@
 function err { >&2 echo "Error: $1"; exit 1; }
 [[ -f /swp ]] && err "Swap already initialized"
 
-sudo fallocate -l 2G /swp
+sudo fallocate -l 6G /swp
 sudo chmod 600 /swp
 sudo mkswap /swp
 sudo swapon /swp
