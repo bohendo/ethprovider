0f28bfc69d5ffbe8604e21ad6b29adcf5ea54e55
Bo Henderson <bohende@gmail.com>
2017-12-13 17:37:30 -0800

    Remove unnecessary etherscan imports

--- js/cryptokitties.js
+++ js/cryptokitties.js
@@ -1,15 +1,10 @@
+import coreAbi from './kittyCore.json'
+import saleAbi from './kittySale.json'
+import sireAbi from './kittySire.json'
 
-////////////////////////////////////////
-// My Imports
-import _core from './kittyCore.js'
-import _sale from './kittySale.js'
-import _sire from './kittySire.js'
-
-////////////////////////////////////////
-// Initialize Variables/Instances
 const firstBlock = 4605167
-const core = eth.contract(_core.abi).at(_core.address)
-const sale = eth.contract(_sale.abi).at(_sale.address)
-const sire = eth.contract(_sire.abi).at(_sire.address)
+const core = eth.contract(coreAbi).at("0x06012c8cf97BEaD5deAe237070F9587f8E7A266d")
+const sale = eth.contract(saleAbi).at("0xb1690C08E213a35Ed9bAb7B318DE14420FB57d8C")
+const sire = eth.contract(sireAbi).at("0xC7af99Fe5513eB6710e6D5f44F9989dA40F27F26")
 
 export { firstBlock, core, sale, sire }
