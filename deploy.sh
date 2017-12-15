74b219dcfa79610e8e74c5684346d338fafce3f0
Bo Henderson <bohende@gmail.com>
2017-12-15 06:46:11 -0800

    Remove docker swarm & complicated build stuff

--- deploy.sh
+++ deploy.sh
@@ -1,43 +1,15 @@
 #!/bin/bash
 
-function err { >&2 echo "Error: $1"; exit 1; }
+docker build -f Dockerfile -t `whoami`/ethprovider:latest -t ethprovider:latest .
 
-target="$1"
-v=$(grep "\"version\"" ./package.json | egrep -o [0-9.]*)
+make deploy
 
-# If we're deploying to localhost things are simpler, just do it
-if [ -z "$target" ]
-then
-  touch js/* && make deploy
-  mkdir -p /tmp/ipc
-  cat docker-compose.yml | sed 's/image: \([a-zA-Z_-]\+\):latest/image: '`whoami`'\/\1:'"$v"'/' > /tmp/docker-compose-ethnode.yml
-  docker stack deploy -c /tmp/docker-compose-ethnode.yml ethnode
-  exit 0
-fi
-
-# Don't deploy if there are uncommitted changes
-if [[ `git status --short | wc -l` -ne 0 ]]
-then
-  err "Commit your changes first"
-fi
-
-# Make sure we can ssh to the machine we're deploying to
-if ! ssh -q $target exit 2> /dev/null
-then
-  err "Couldn't open an ssh connection to $target"
-fi
-
-# Make sure project gets rebuilt as the production version
-touch js/* && make deploy
-ssh $target mkdir -p /tmp/ipc
-
-cat docker-compose.yml |\
-  sed 's/image: \([a-zA-Z_-]\+\):latest/image: '`whoami`'\/\1:'"$v"'/' |\
-  ssh $target "cat - > ~/docker-compose-ethnode.yml"
-
-ssh $target docker pull `whoami`/geth:$v
-
-ssh $target 'bash -s' <<EOF
-docker stack deploy -c docker-compose-ethnode.yml ethnode
-EOF
+docker service create \
+  --name "ethprovider" \
+  --mode "global" \
+  --secret "eth_keyfile" \
+  --mount "type=volume,source=ethnode_chaindata,destination=/root/.ethereum/geth" \
+  --mount "type=volume,source=ipc,destination=/tmp/ipc" \
+  --detach \
+  ethprovider:latest
 
