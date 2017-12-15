75a8b95bca24922cb2e7b9a5168500057ae00a49
Bo Henderson <bohende@gmail.com>
2017-12-15 06:51:11 -0800

    Rename volumes

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,12 @@
 
 docker build -f Dockerfile -t `whoami`/ethprovider:latest -t ethprovider:latest .
 
-make deploy
-
 docker service create \
   --name "ethprovider" \
   --mode "global" \
   --secret "eth_keyfile" \
-  --mount "type=volume,source=ethnode_chaindata,destination=/root/.ethereum/geth" \
-  --mount "type=volume,source=ipc,destination=/tmp/ipc" \
+  --mount "type=volume,source=ethprovider_chaindata,destination=/root/.ethereum/geth" \
+  --mount "type=volume,source=ethprovider_ipc,destination=/tmp/ipc" \
   --detach \
   ethprovider:latest
 
