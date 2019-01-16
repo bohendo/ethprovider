16cdbd406df18a22e263cfda767a46af97d6d302
Bo Henderson <bohende@gmail.com>
2019-01-16 22:58:32 +0530

    increment version to 1.1.1

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-version=1.1.0
+version=1.1.1
 project=ethprovider
 registry=docker.io/$(shell whoami)
 mode=manual # simple or manual
--- ops/deploy.sh
+++ ops/deploy.sh
@@ -4,7 +4,7 @@ set -e
 ########################################
 ## Config
 
-version=1.1.0
+version=1.1.1
 name="`whoami`"
 cache="4096"
 data_dir="/root/eth"
