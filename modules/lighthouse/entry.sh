00cdcfefd715c645ba74bffd3df94303949ccd6a
bohendo <bohende@gmail.com>
2020-11-28 17:16:01 +0530

    🔧 provide password

--- modules/lighthouse/entry.sh
+++ modules/lighthouse/entry.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-lighthouse --network mainnet account validator import --keystore keystore.json
+lighthouse \
+  --network mainnet \
+  account \
+  validator \
+  import \
+    --keystore keystore.json \
+    --stdin-inputs \
+    <<<"$(cat /run/secrets/validator_keystore_password)"
 
 exec lighthouse beacon --http --http-address=0.0.0.0
