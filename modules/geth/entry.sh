e85c5e611343845d1c231740fdaba1ee4f9940b1
bohendo <bohende@gmail.com>
2020-11-29 20:25:49 +0530

    🔧 eth1 expose net api too

--- modules/geth/entry.sh
+++ modules/geth/entry.sh
@@ -9,11 +9,11 @@ exec geth \
   --http \
   --http.addr=0.0.0.0 \
   --http.port=8545 \
-  --http.api=eth \
+  --http.api=eth,net \
   --http.corsdomain=* \
   --http.vhosts=* \
   --ws \
   --ws.addr=0.0.0.0 \
   --ws.port=8546 \
-  --ws.api=eth \
+  --ws.api=eth,net \
   --ws.origins=*
