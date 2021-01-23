53dbfeb48c9e88a5ce4f3781854513b4d64b7dd7
bohendo <bohendo@pm.me>
2021-01-23 18:42:20 +0530

    init search & replace scripts

---
+++ ops/search.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+target="$1";
+shift;
+
+if [[ -z "$target" ]]
+then echo "One arg required: bash ops/search.sh <target>" && exit 1
+fi
+
+grep "$@" --exclude=*.swp --exclude=*.pdf --color=auto -r "$target" \
+  Makefile \
+  modules/*/* \
+  ops
