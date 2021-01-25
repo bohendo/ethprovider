cadd5108306b4836faa3dc9b01505213fc6fe7f1
bohendo <bohendo@pm.me>
2021-01-25 17:28:52 +0530

    consolidate eth network env vars

--- ops/start.sh
+++ ops/start.sh
@@ -29,36 +29,43 @@ if [[ -f ".env" ]]; then source ".env"; fi
 ETH_1_CACHE="${ETH_1_CACHE:-2048}"
 ETH_1_DATADIR="${ETH_1_DATADIR:-geth}"
 ETH_1_LIGHT_SERVE="${ETH_1_LIGHT_SERVE:-20}"
-ETH_1_NETWORK="${ETH_1_NETWORK:-goerli}"
 
 ETH_2_DATADIR="${ETH_2_DATADIR:-lighthouse}"
 ETH_2_ETH1_URL="${ETH_2_ETH1_URL:-http://eth1:$eth1_http_port}"
 ETH_2_KEYSTORE="${ETH_2_KEYSTORE:-validator_keys}"
-ETH_2_NETWORK="${ETH_2_NETWORK:-pyrmont}"
 
 ETH_API_KEY="${ETH_API_KEY:-abc123}"
 ETH_DATA_ROOT="${ETH_DATA_ROOT:-$root/.data}"
 ETH_DOMAINNAME="${ETH_DOMAINNAME:-}"
+ETH_MAINNET="${ETH_MAINNET:-false}"
 
 echo "Starting eth stack in env:"
 echo "- ETH_1_CACHE=$ETH_1_CACHE"
 echo "- ETH_1_LIGHT_SERVE=$ETH_1_LIGHT_SERVE"
 echo "- ETH_1_DATADIR=$ETH_1_DATADIR"
-echo "- ETH_1_NETWORK=$ETH_1_NETWORK"
 
 echo "- ETH_2_DATADIR=$ETH_2_DATADIR"
 echo "- ETH_2_ETH1_URL=$ETH_2_ETH1_URL"
 echo "- ETH_2_KEYSTORE=$ETH_2_KEYSTORE"
-echo "- ETH_2_NETWORK=$ETH_2_NETWORK"
 
 echo "- ETH_API_KEY=$ETH_API_KEY"
 echo "- ETH_DATA_ROOT=$ETH_DATA_ROOT"
 echo "- ETH_DOMAINNAME=$ETH_DOMAINNAME"
+echo "- ETH_MAINNET=$ETH_MAINNET"
 
 ########################################
 ## Configure internal vars
 
 mkdir -p "$ETH_DATA_ROOT/$ETH_1_DATADIR" "$ETH_DATA_ROOT/$ETH_2_DATADIR"
+
+if [[ "$ETH_MAINNET" == "true" ]]
+then
+  eth1_network="mainnet"
+  eth2_network="mainnet"
+else
+  eth1_network="goerli"
+  eth2_network="pyrmont"
+fi
 
 proxy_image="${stack}_proxy:v$(grep proxy versions | awk -F '=' '{print $2}')"
 geth_image="${stack}_geth:v$(grep "geth" versions | awk -F '=' '{print $2}')"
@@ -130,8 +137,8 @@ services:
     environment:
       ETH_1_CACHE: '$ETH_1_CACHE'
       ETH_1_LIGHT_SERVE: '$ETH_1_LIGHT_SERVE'
-      ETH_1_DATADIR: '$ETH_1_DATADIR/$ETH_1_NETWORK'
-      ETH_1_NETWORK: '$ETH_1_NETWORK'
+      ETH_1_DATADIR: '$ETH_1_DATADIR/$eth1_network'
+      ETH_1_NETWORK: '$eth1_network'
       ETH_1_HTTP_PORT: '$eth1_http_port'
       ETH_1_WS_PORT: '$eth1_ws_port'
     $logging
@@ -143,10 +150,10 @@ services:
   beacon:
     image: $lighthouse_image
     environment:
-      ETH_2_DATADIR: '$ETH_2_DATADIR/$ETH_2_NETWORK'
+      ETH_2_DATADIR: '$ETH_2_DATADIR/$eth2_network'
       ETH_2_ETH1_URL: '$ETH_2_ETH1_URL'
       ETH_2_MODULE: 'beacon'
-      ETH_2_NETWORK: '$ETH_2_NETWORK'
+      ETH_2_NETWORK: '$eth2_network'
       ETH_2_INTERNAL_PORT: '$beacon_internal_port'
     $logging
     volumes:
@@ -156,10 +163,10 @@ services:
     image: $lighthouse_image
     environment:
       ETH_2_BEACON_URL: 'http://beacon:$beacon_internal_port'
-      ETH_2_DATADIR: '$ETH_2_DATADIR/$ETH_2_NETWORK'
+      ETH_2_DATADIR: '$ETH_2_DATADIR/$eth2_network'
       ETH_2_KEYSTORE: '$ETH_2_KEYSTORE'
       ETH_2_MODULE: 'validator'
-      ETH_2_NETWORK: '$ETH_2_NETWORK'
+      ETH_2_NETWORK: '$eth2_network'
       ETH_2_PASSWORD_FILE: '/run/secrets/$password_secret'
     $logging
     secrets:
